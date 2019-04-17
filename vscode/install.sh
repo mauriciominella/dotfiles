8e1bf0bef80f5920b6523420a139dcb6129a2543
Mauricio Minella <mauriciominella@gmail.com>
2019-04-17 10:29:00 +0200

    The settings location for vscode differs from mac and linux. Therefore we need two different sets of commands to create the symlinks

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,2 +1,11 @@
-ln -s ~/git/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
-ln -s ~/git/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+  # Mac OS X platform        
+  ln -s ~/git/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+  ln -s ~/git/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  # GNU/Linux platform
+  ln -s ~/git/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
+  ln -s ~/git/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+fi
